1ab80de4fd47f11860ce5f3d1f47edbc6e1bdca1
rayliuxh <xh_liu@sio.org.cn>
2020-08-13 17:03:10 +0800

    rename ROMSVOR

--- ROMSVOR/vort_2d_ROMSVOR.m
+++
@@ -1,5 +0,0 @@
-function [dvdx_2d,dudy_2d] = vort_2d_ROMSVOR(u2d,v2d,pm_p,pn_p)
-%%% get dvbar/dx and dubar/dy for the 2d vorticity equation
-
-dvdx_2d = (v2d(2:end,:,:)-v2d(1:end-1,:,:)).*pm_p;
-dudy_2d = (u2d(:,2:end,:)-u2d(:,1:end-1,:)).*pn_p;
--- ROMSVOR/vort_2dtrans_ROMSVOR.m
+++
@@ -1,8 +0,0 @@
-function [dvdx_t_2d,dudy_t_2d] = vort_2dtrans_ROMSVOR(u2d,v2d,pm_p,pn_p,hdep_u,hdep_v,hdep_p)
-%%% get 1/D*d(vbar*D)/dx and 1/D*d(ubar*D)/dy 
-%%% for the 2d transport vorticity equation
-
-u2d_trans = u2d.*hdep_u; v2d_trans = v2d.*hdep_v;
-
-dvdx_t_2d = (v2d_trans(2:end,:,:)-v2d_trans(1:end-1,:,:)).*pm_p./hdep_p;
-dudy_t_2d = (u2d_trans(:,2:end,:)-u2d_trans(:,1:end-1,:)).*pn_p./hdep_p;
